6a1461c76085c73263287eee62ee4ec559c006ba
Martin Krutil <martin.krutil@email.cz>
2024-11-09 19:21:35 +0100

    renamed

--- playfair.py
+++ playfair.py
@@ -1,37 +1,40 @@
+import sys
 import unicodedata
+import PySide6.QtWidgets as qtw
+import PySide6.QtCore as qtc
 
 config = {
-    "key": "keynzu",
     "alphabet": "abcdefghijklmnopqrstuvwxyz",
     "substitution_characters": ('x', 'q'),
     "replaced_characters": ('j', 'i')
 }
 
 def main():
-    open_text = "Open / text $$ sample 12:45 j ěščřžýáíé"
-    encrypted_text = encrypt(config["key"], config["alphabet"], open_text)
-    decrypted_text = decrypt(config["key"], config["alphabet"], encrypted_text)
-
-    print(f"open text:\t{open_text}")
-    print(f"formatted text:\t{format_text(open_text)}")
-    print(f"encrypted text:\t{encrypted_text}")
-    print(f"decrypted text:\t{decrypted_text}")
+    app = qtw.QApplication(sys.argv)
+    window = CipherApp()
+    window.show()
+    sys.exit(app.exec())
 
 def format_text(input_text: str) -> str:
     formatted_text = [
         c.lower() for c in input_text
-        if c.isalnum() or c.isspace()
+        if c.isalpha() or c.isspace()
+    ]
+
+    formatted_text = [
+        c for i, c in enumerate(formatted_text)
+        if c != ' ' or (i == 0 or formatted_text[i - 1] != ' ')
     ]
 
     for i in range(len(formatted_text)):
-        if formatted_text[i].isspace() or formatted_text[i].isnumeric(): 
+        if formatted_text[i].isspace():
             formatted_text[i] = ('x' + unicodedata.name(formatted_text[i]).replace(' ', '') + 'x').lower()
 
     formatted_text = [
         c for c in unicodedata.normalize('NFD', ''.join(formatted_text))
         if unicodedata.category(c) != 'Mn'
     ]
-    
+
     i = 0
     while i < len(formatted_text) - 1:
         if formatted_text[i] == formatted_text[i + 1]:
@@ -49,16 +52,14 @@ def format_text(input_text: str) -> str:
 
     return ''.join(formatted_text)
 
-def create_key_matrix(key: str, alphabet: str) ->  list[list[str]]:
+def create_key_matrix(key: str, alphabet: str) -> list:
     key = key.lower().replace('j', 'i')
-    key = sorted(set(key), key = lambda x: key.index(x)) 
+    key = ''.join([c for c in unicodedata.normalize('NFD', key) if c.isalpha() and unicodedata.category(c) != 'Mn'])
+    key = sorted(set(key), key=lambda x: key.index(x))
     key += ''.join([x for x in alphabet.replace('j', '') if x not in key])
     return [list(key[i:i + 5]) for i in range(0, 25, 5)]
- 
-def print_key_matrix(matrix: list[list[str]]) -> None:
-    for i in range(0, 5): print(matrix[i])
 
-def character_position(matrix: list[list[str]], character: str) -> tuple[int, int]:
+def character_position(matrix: list, character: str) -> tuple:
     if character == 'j': character = 'i'
     for i, row in enumerate(matrix):
         if character in row: return (i, row.index(character))
@@ -68,41 +69,148 @@ def encrypt(key: str, alphabet: str, input_text: str) -> str:
     matrix = create_key_matrix(key, alphabet)
     formatted_text = format_text(input_text)
     encrypted_text = []
-    
+
     for i in range(0, len(formatted_text), 2):
         char_a = character_position(matrix, formatted_text[i])
-        char_b = character_position(matrix, formatted_text[i+1])
-        encrypted_pair = None
-
+        char_b = character_position(matrix, formatted_text[i + 1])
         if char_a[0] == char_b[0]:
-            encrypted_pair = matrix[char_a[0]][(char_a[1] + 1) % 5] + matrix[char_b[0]][(char_b[1] + 1) % 5]
+            encrypted_text.append(matrix[char_a[0]][(char_a[1] + 1) % 5] + matrix[char_b[0]][(char_b[1] + 1) % 5])
         elif char_a[1] == char_b[1]:
-            encrypted_pair = matrix[(char_a[0] + 1) % 5][char_a[1]] + matrix[(char_b[0] + 1) % 5][char_b[1]]
+            encrypted_text.append(matrix[(char_a[0] + 1) % 5][char_a[1]] + matrix[(char_b[0] + 1) % 5][char_b[1]])
         else:
-            encrypted_pair = matrix[char_a[0]][char_b[1]] + matrix[char_b[0]][char_a[1]]
+            encrypted_text.append(matrix[char_a[0]][char_b[1]] + matrix[char_b[0]][char_a[1]])
 
-        encrypted_text.append(encrypted_pair)
-    
     return ''.join(encrypted_text)
 
 def decrypt(key: str, alphabet: str, encrypted_text: str) -> str:
     matrix = create_key_matrix(key, alphabet)
+    formatted_text = encrypted_text.lower()
+    if len(formatted_text) % 2 != 0:
+        sub_char = config["substitution_characters"][0]
+        if formatted_text[-1] == sub_char:
+            sub_char = config["substitution_characters"][1]
+        formatted_text.append(sub_char)
+
     decrypted_text = []
-    
-    for i in range(0, len(encrypted_text), 2):
-        char_a = character_position(matrix, encrypted_text[i])
-        char_b = character_position(matrix, encrypted_text[i+1])
-        decrypted_pair = None
 
+    for i in range(0, len(formatted_text), 2):
+        char_a = character_position(matrix, formatted_text[i])
+        char_b = character_position(matrix, formatted_text[i + 1])
         if char_a[0] == char_b[0]:
-            decrypted_pair = matrix[char_a[0]][(char_a[1] - 1) % 5] + matrix[char_b[0]][(char_b[1] - 1) % 5]
+            decrypted_text.append(matrix[char_a[0]][(char_a[1] - 1) % 5] + matrix[char_b[0]][(char_b[1] - 1) % 5])
         elif char_a[1] == char_b[1]:
-            decrypted_pair = matrix[(char_a[0] - 1) % 5][char_a[1]] + matrix[(char_b[0] - 1) % 5][char_b[1]]
-        else: 
-            decrypted_pair = matrix[char_a[0]][char_b[1]] + matrix[char_b[0]][char_a[1]]
+            decrypted_text.append(matrix[(char_a[0] - 1) % 5][char_a[1]] + matrix[(char_b[0] - 1) % 5][char_b[1]])
+        else:
+            decrypted_text.append(matrix[char_a[0]][char_b[1]] + matrix[char_b[0]][char_a[1]])
 
-        decrypted_text.append(decrypted_pair)
-    
-    return ''.join(decrypted_text)
+    space = 'x' + unicodedata.name(' ') + 'x'
+    return ''.join(decrypted_text).replace(space.lower(), ' ')
 
-if __name__ == '__main__': main()
+class CipherApp(qtw.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("playfair")
+        self.setGeometry(200, 200, 600, 400)
+        self.central_widget = qtw.QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.layout = qtw.QVBoxLayout()
+        self.central_widget.setLayout(self.layout)
+
+        self.key_label = qtw.QLabel("key")
+        self.layout.addWidget(self.key_label)
+        self.key_text = qtw.QLineEdit()
+        self.layout.addWidget(self.key_text)
+
+        self.key_matrix_table = qtw.QTableWidget(5, 5)
+        self.key_matrix_table.setFixedSize(150, 150)
+        self.key_matrix_table.setEditTriggers(qtw.QTableWidget.NoEditTriggers)
+        self.key_matrix_table.setHorizontalScrollBarPolicy(qtc.Qt.ScrollBarAlwaysOff)
+        self.key_matrix_table.setVerticalScrollBarPolicy(qtc.Qt.ScrollBarAlwaysOff)
+        self.key_matrix_table.horizontalHeader().setVisible(False)
+        self.key_matrix_table.verticalHeader().setVisible(False)
+
+        for i in range(5):
+            self.key_matrix_table.setColumnWidth(i, 30)
+            self.key_matrix_table.setRowHeight(i, 30)
+
+        self.input_label = qtw.QLabel("input")
+        self.layout.addWidget(self.input_label)
+        self.input_text = qtw.QTextEdit()
+        self.layout.addWidget(self.input_text)
+
+        self.formatted_input_label = qtw.QLabel("formatted input")
+        self.layout.addWidget(self.formatted_input_label)
+        self.formatted_input_text = qtw.QTextEdit()
+        self.formatted_input_text.setReadOnly(True)
+        self.layout.addWidget(self.formatted_input_text)
+
+        self.v_box_layout = qtw.QHBoxLayout()
+
+        self.output_label = qtw.QLabel("output")
+        self.layout.addWidget(self.output_label)
+        self.output_text = qtw.QTextEdit()
+        self.output_text.setReadOnly(True)
+        self.v_box_layout.addWidget(self.output_text)
+        
+        self.v_box_layout.addWidget(self.key_matrix_table)
+        self.layout.addLayout(self.v_box_layout)
+        self.update_key_matrix() 
+
+        self.button_layout = qtw.QHBoxLayout()
+        
+        self.encrypt_button = qtw.QPushButton("encrypt")
+        self.encrypt_button.clicked.connect(self.handle_encrypt)
+        self.button_layout.addWidget(self.encrypt_button)
+        
+        self.decrypt_button = qtw.QPushButton("decrypt")
+        self.decrypt_button.clicked.connect(self.handle_decrypt)
+        self.button_layout.addWidget(self.decrypt_button)
+        
+        self.layout.addLayout(self.button_layout)
+
+    def update_key_matrix(self):
+        matrix = create_key_matrix(self.key_text.text(), config["alphabet"])
+        
+        for i in range(5):
+            for j in range(5):
+                item = qtw.QTableWidgetItem(matrix[i][j])
+                item.setTextAlignment(qtc.Qt.AlignCenter)
+                self.key_matrix_table.setItem(i, j, item)
+
+    def handle_encrypt(self):
+        self.update_key_matrix()
+        encrypted_text = encrypt(self.key_text.text(), config["alphabet"], self.input_text.toPlainText())
+        encrypted_text = ''.join([
+                c if i == 0 or i % 5 != 0
+                else f' {c}'
+                for i, c in enumerate(encrypted_text)
+        ])
+        self.output_text.setText(encrypted_text.upper())
+        formatted_input = format_text(self.input_text.toPlainText()).upper()
+        formatted_input = ''.join([
+                c if i == 0 or i % 2 != 0
+                else f' {c}'
+                for i, c in enumerate(formatted_input)
+        ])
+        self.formatted_input_text.setText(formatted_input)
+
+    def handle_decrypt(self):
+        self.update_key_matrix()
+        decrypted_text = decrypt(self.key_text.text(), config["alphabet"], self.input_text.toPlainText())
+        decrypted_text = ''.join([
+                c if i == 0 or i % 5 != 0
+                else f' {c}'
+                for i, c in enumerate(decrypted_text)
+        ])
+        self.output_text.setText(decrypted_text.upper())
+        formatted_input = format_text(self.input_text.toPlainText()).upper()
+        self.formatted_input_text.setText(formatted_input)
+        formatted_input = self.input_text.toPlainText().upper()
+        formatted_input = ''.join([
+                c if i == 0 or i % 2 != 0
+                else f' {c}'
+                for i, c in enumerate(formatted_input)
+        ])
+        self.formatted_input_text.setText(formatted_input)
+
+if __name__ == "__main__": main()
