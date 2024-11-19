dc9f04286698cb00adcceed7ab965c246b4368fa
kuruteiru <martin.krutil@email.cz>
2024-11-19 02:43:53 +0100

    adfgvx

--- ciphers/adfgvx.py
+++ ciphers/adfgvx.py
@@ -2,12 +2,11 @@ import unicodedata
 import sys
 
 config = {
-    "alphabet": "abcdefghijklmnopqrstuvwxyz",
-    "substitution_characters": ('x', 'q'),
-    "replaced_characters": ('j', 'i')
+    "alphabet": "abcdefghijklmnopqrstuvwxyz0123456789",
 }
 
-def main(): 
+def main():
+    print("adfgvx")
     if len(sys.argv) == 1: return
     match sys.argv[1]:
         case "-e" | "-encrypt": print('encrypt')
@@ -19,92 +18,82 @@ def main():
 def format_text(input_text: str) -> str:
     formatted_text = [
         c.lower() for c in input_text
-        if c.isalpha() or c.isspace()
+        if c.isalnum() or c.isspace()
     ]
-
-    formatted_text = [
-        c for i, c in enumerate(formatted_text)
-        if c != ' ' or (i == 0 or formatted_text[i - 1] != ' ')
-    ]
-
-    for i in range(len(formatted_text)):
-        if formatted_text[i].isspace():
-            formatted_text[i] = ('x' + unicodedata.name(formatted_text[i]).replace(' ', '') + 'x').lower()
-
-    formatted_text = [
+    
+    formatted_text = ''.join([
         c for c in unicodedata.normalize('NFD', ''.join(formatted_text))
         if unicodedata.category(c) != 'Mn'
-    ]
+    ])
+    
+    formatted_text = formatted_text.replace('j', 'i')
+    return formatted_text
 
-    i = 0
-    while i < len(formatted_text) - 1:
-        if formatted_text[i] == formatted_text[i + 1]:
-            sub_char = config["substitution_characters"][0]
-            if formatted_text[i] == sub_char:
-                sub_char = config["substitution_characters"][1]
-            formatted_text.insert(i + 1, sub_char)
-        i += 1
+def create_key_matrix(key: str) -> list[list[str]]:
+    key = format_text(key)
+    key_matrix = sorted(set(key), key=lambda x: key.index(x))
+    key_matrix += [x for x in config["alphabet"] if x not in key_matrix]
+    return [key_matrix[i:i+6] for i in range(0, 36, 6)]
 
-    if len(formatted_text) % 2 != 0:
-        sub_char = config["substitution_characters"][0]
-        if formatted_text[-1] == sub_char:
-            sub_char = config["substitution_characters"][1]
-        formatted_text.append(sub_char)
+def create_columnar_key(key: str) -> list[int]:
+    key = format_text(key)
+    positions = list(range(len(key)))
+    pairs = sorted(zip(key, positions))
+    sorted_positions = list(zip(*pairs))[1]
+    return sorted_positions
 
-    return ''.join(formatted_text)
+def get_coordinates(key_matrix: list[list[str]], char: str) -> tuple[str, str]:
+    coordinates = "ADFGVX"
+    for i, row in enumerate(key_matrix):
+        if char in row:
+            return (coordinates[i], coordinates[row.index(char)])
+    return ('', '')
 
-def create_key_matrix(key: str, alphabet: str) -> list[list[str]]:
-    key = key.lower().replace('j', 'i')
-    key_matrix = [c for c in unicodedata.normalize('NFD', key) if c.isalpha() and unicodedata.category(c) != 'Mn']
-    key_matrix = sorted(set(key_matrix), key = lambda x: key_matrix.index(x))
-    key_matrix += [x for x in alphabet.replace('j', '') if x not in key_matrix]
-    return [key_matrix[i:i+5] for i in range(0, 25, 5)]
-
-def character_position(matrix: list, character: str) -> tuple:
-    if character == 'j': character = 'i'
-    for i, row in enumerate(matrix):
-        if character in row: return (i, row.index(character))
-    return (-1, -1)
-
-def encrypt(key: str, alphabet: str, input_text: str) -> str:
-    matrix = create_key_matrix(key, alphabet)
+def encrypt(key1: str, key2: str, input_text: str) -> str:
+    matrix = create_key_matrix(key1)
     formatted_text = format_text(input_text)
-    encrypted_text = []
+    coordinates = ""
+    for char in formatted_text:
+        coords = get_coordinates(matrix, char)
+        coordinates += coords[0] + coords[1]
+    
+    key_order = create_columnar_key(key2)
+    key_length = len(key2)
+    
+    if len(coordinates) % key_length != 0:
+        x_coords = get_coordinates(matrix, 'x')
+        while len(coordinates) % key_length != 0:
+            coordinates += x_coords[0] + x_coords[1]
+    
+    columns = [coordinates[i::key_length] for i in range(key_length)]
+    return ''.join(columns[i] for i in key_order)
 
-    for i in range(0, len(formatted_text), 2):
-        char_a = character_position(matrix, formatted_text[i])
-        char_b = character_position(matrix, formatted_text[i + 1])
-        if char_a[0] == char_b[0]:
-            encrypted_text.append(matrix[char_a[0]][(char_a[1] + 1) % 5] + matrix[char_b[0]][(char_b[1] + 1) % 5])
-        elif char_a[1] == char_b[1]:
-            encrypted_text.append(matrix[(char_a[0] + 1) % 5][char_a[1]] + matrix[(char_b[0] + 1) % 5][char_b[1]])
-        else:
-            encrypted_text.append(matrix[char_a[0]][char_b[1]] + matrix[char_b[0]][char_a[1]])
+def decrypt(key1: str, key2: str, encrypted_text: str) -> str:
+    matrix = create_key_matrix(key1)
+    key_order = create_columnar_key(key2)
+    key_length = len(key2)
+    col_length = len(encrypted_text) // key_length
+    columns = [''] * key_length
 
-    return ''.join(encrypted_text)
-
-def decrypt(key: str, alphabet: str, encrypted_text: str) -> str:
-    matrix = create_key_matrix(key, alphabet)
-    formatted_text = encrypted_text.lower()
-    decrypted_text = []
-
-    if len(formatted_text) % 2 != 0:
-        sub_char = config["substitution_characters"][0]
-        if formatted_text[-1] == sub_char:
-            sub_char = config["substitution_characters"][1]
-        formatted_text += sub_char
-
-    for i in range(0, len(formatted_text), 2):
-        char_a = character_position(matrix, formatted_text[i])
-        char_b = character_position(matrix, formatted_text[i + 1])
-        if char_a[0] == char_b[0]:
-            decrypted_text.append(matrix[char_a[0]][(char_a[1] - 1) % 5] + matrix[char_b[0]][(char_b[1] - 1) % 5])
-        elif char_a[1] == char_b[1]:
-            decrypted_text.append(matrix[(char_a[0] - 1) % 5][char_a[1]] + matrix[(char_b[0] - 1) % 5][char_b[1]])
-        else:
-            decrypted_text.append(matrix[char_a[0]][char_b[1]] + matrix[char_b[0]][char_a[1]])
-
-    space = 'x' + unicodedata.name(' ') + 'x'
-    return ''.join(decrypted_text).replace(space.lower(), ' ')
+    pos = 0
+    for i in range(col_length):
+        for j in key_order:
+            if pos < len(encrypted_text):
+                columns[j] += encrypted_text[pos]
+                pos += 1
+    
+    coordinates = ""
+    for i in range(col_length):
+        for col in columns:
+            if i < len(col):
+                coordinates += col[i]
+    
+    decrypted_text = ""
+    for i in range(0, len(coordinates), 2):
+        row_idx = "ADFGVX".index(coordinates[i])
+        col_idx = "ADFGVX".index(coordinates[i + 1])
+        decrypted_text += matrix[row_idx][col_idx]
+    
+    return decrypted_text
 
 if __name__ == "__main__": main()
