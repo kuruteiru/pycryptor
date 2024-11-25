c12b4d599405e5a85ae14a63beed241d7b3a875d
kuruteiru <martin.krutil@email.cz>
2024-11-25 22:15:19 +0100

    fixed, using text_formatter.py, added more typehints

--- ciphers/playfair.py
+++ ciphers/playfair.py
@@ -1,15 +1,15 @@
+from typing import Any
+import text_formatter as tf
 import PySide6.QtWidgets as qtw
 import PySide6.QtCore as qtc
 import unicodedata
 import sys
 
-config = {
-    "alphabet": "abcdefghijklmnopqrstuvwxyz",
-    "substitution_characters": ('x', 'q'),
-    "replaced_characters": ('j', 'i')
+config: dict[str, Any] = {
+    "alphabet": "abcdefghiklmnopqrstuvwxyz",
 }
 
-def main(): 
+def main() -> None: 
     if len(sys.argv) == 1:
         app = qtw.QApplication(sys.argv)
         window = App()
@@ -24,47 +24,20 @@ def main():
         case _: print("unknown argument")
 
 def format_text(input_text: str) -> str:
-    formatted_text = [
-        c.lower() for c in input_text
-        if c.isalpha() or c.isspace()
-    ]
+    char_map: dict[str, str] = tf.get_nonrepeating_char_map()
 
-    formatted_text = [
-        c for i, c in enumerate(formatted_text)
-        if c != ' ' or (i == 0 or formatted_text[i - 1] != ' ')
-    ]
+    formatted_text: str = ''
+    formatted_text = tf.normalize_text(input_text, char_map = char_map)
+    formatted_text = tf.format_repeating_chars(formatted_text)
+    formatted_text = tf.even_length(formatted_text)
 
-    for i in range(len(formatted_text)):
-        if formatted_text[i].isspace():
-            formatted_text[i] = ('x' + unicodedata.name(formatted_text[i]).replace(' ', '') + 'x').lower()
-
-    formatted_text = [
-        c for c in unicodedata.normalize('NFD', ''.join(formatted_text))
-        if unicodedata.category(c) != 'Mn'
-    ]
-
-    i = 0
-    while i < len(formatted_text) - 1:
-        if formatted_text[i] == formatted_text[i + 1]:
-            sub_char = config["substitution_characters"][0]
-            if formatted_text[i] == sub_char:
-                sub_char = config["substitution_characters"][1]
-            formatted_text.insert(i + 1, sub_char)
-        i += 1
-
-    if len(formatted_text) % 2 != 0:
-        sub_char = config["substitution_characters"][0]
-        if formatted_text[-1] == sub_char:
-            sub_char = config["substitution_characters"][1]
-        formatted_text.append(sub_char)
-
-    return ''.join(formatted_text)
+    return formatted_text
 
 def create_key_matrix(key: str, alphabet: str) -> list[list[str]]:
     key = key.lower().replace('j', 'i')
-    key_matrix = [c for c in unicodedata.normalize('NFD', key) if c.isalpha() and unicodedata.category(c) != 'Mn']
+    key_matrix: list[str] = [c for c in unicodedata.normalize('NFD', key) if c.isalpha() and unicodedata.category(c) != 'Mn']
     key_matrix = sorted(set(key_matrix), key = lambda x: key_matrix.index(x))
-    key_matrix += [x for x in alphabet.replace('j', '') if x not in key_matrix]
+    key_matrix += [x for x in alphabet if x not in key_matrix]
     return [key_matrix[i:i+5] for i in range(0, 25, 5)]
 
 def get_coordinates(matrix: list[list[str]], char: str) -> tuple[int, int]:
@@ -74,13 +47,14 @@ def get_coordinates(matrix: list[list[str]], char: str) -> tuple[int, int]:
     return (-1, -1)
 
 def encrypt(key: str, alphabet: str, input_text: str) -> str:
-    matrix = create_key_matrix(key, alphabet)
-    formatted_text = format_text(input_text)
-    encrypted_text = []
+    matrix: list[list[str]] = create_key_matrix(key, alphabet)
+    formatted_text: str = format_text(input_text)
+    encrypted_text: list[str] = []
 
     for i in range(0, len(formatted_text), 2):
-        char_a = get_coordinates(matrix, formatted_text[i])
-        char_b = get_coordinates(matrix, formatted_text[i + 1])
+        char_a: tuple[int, int] = get_coordinates(matrix, formatted_text[i])
+        char_b: tuple[int, int] = get_coordinates(matrix, formatted_text[i+1])
+
         if char_a[0] == char_b[0]:
             encrypted_text.append(matrix[char_a[0]][(char_a[1] + 1) % 5] + matrix[char_b[0]][(char_b[1] + 1) % 5])
         elif char_a[1] == char_b[1]:
@@ -91,19 +65,14 @@ def encrypt(key: str, alphabet: str, input_text: str) -> str:
     return ''.join(encrypted_text)
 
 def decrypt(key: str, alphabet: str, encrypted_text: str) -> str:
-    matrix = create_key_matrix(key, alphabet)
-    formatted_text = encrypted_text.lower()
-    decrypted_text = []
-
-    if len(formatted_text) % 2 != 0:
-        sub_char = config["substitution_characters"][0]
-        if formatted_text[-1] == sub_char:
-            sub_char = config["substitution_characters"][1]
-        formatted_text += sub_char
+    matrix: list[list[str]] = create_key_matrix(key, alphabet)
+    formatted_text: str = tf.even_length(encrypted_text.lower())
+    decrypted_text: list[str] = []
 
     for i in range(0, len(formatted_text), 2):
-        char_a = get_coordinates(matrix, formatted_text[i])
-        char_b = get_coordinates(matrix, formatted_text[i + 1])
+        char_a: tuple[int, int] = get_coordinates(matrix, formatted_text[i])
+        char_b: tuple[int, int] = get_coordinates(matrix, formatted_text[i+1])
+
         if char_a[0] == char_b[0]:
             decrypted_text.append(matrix[char_a[0]][(char_a[1] - 1) % 5] + matrix[char_b[0]][(char_b[1] - 1) % 5])
         elif char_a[1] == char_b[1]:
@@ -111,28 +80,30 @@ def decrypt(key: str, alphabet: str, encrypted_text: str) -> str:
         else:
             decrypted_text.append(matrix[char_a[0]][char_b[1]] + matrix[char_b[0]][char_a[1]])
 
-    space = 'x' + unicodedata.name(' ') + 'x'
-    return ''.join(decrypted_text).replace(space.lower(), ' ')
+    decrypted_text = list(tf.revert_repeating_chars(''.join(decrypted_text)))
+    decrypted_text = list(tf.replace_char_map_values(''.join(decrypted_text), tf.get_nonrepeating_char_map()))
+    decrypted_text = list(tf.revert_even_length(''.join(decrypted_text)))
+
+    return ''.join(decrypted_text)
 
 class App(qtw.QMainWindow):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("playfair")
-        # self.setGeometry(200, 200, 600, 400)
-        self.central_widget = qtw.QWidget()
+        self.central_widget: qtw.QWidget = qtw.QWidget()
         self.setCentralWidget(self.central_widget)
-        self.main_layout = qtw.QVBoxLayout()
+        self.main_layout: qtw.QVBoxLayout = qtw.QVBoxLayout()
         self.central_widget.setLayout(self.main_layout)
 
-        self.key_label = qtw.QLabel("key")
+        self.key_label: qtw.QLabel = qtw.QLabel("key")
         self.main_layout.addWidget(self.key_label)
-        self.key_text = qtw.QLineEdit()
+        self.key_text: qtw.QLineEdit = qtw.QLineEdit()
         self.main_layout.addWidget(self.key_text)
 
-        self.matrix_size = 5
-        self.cell_size = 30
+        self.matrix_size: int = 5
+        self.cell_size: int = 30
 
-        self.key_matrix_table = qtw.QTableWidget(self.matrix_size, self.matrix_size)
+        self.key_matrix_table: qtw.QTableWidget = qtw.QTableWidget(self.matrix_size, self.matrix_size)
         self.key_matrix_table.setFixedSize(self.matrix_size * self.cell_size + 40, self.matrix_size * self.cell_size)
         self.key_matrix_table.setEditTriggers(qtw.QTableWidget.EditTrigger.NoEditTriggers)
         self.key_matrix_table.setHorizontalScrollBarPolicy(qtc.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
@@ -144,83 +115,75 @@ class App(qtw.QMainWindow):
             self.key_matrix_table.setColumnWidth(i, self.cell_size)
             self.key_matrix_table.setRowHeight(i, self.cell_size)
 
-        self.input_label = qtw.QLabel("input")
+        self.input_label: qtw.QLabel = qtw.QLabel("input")
         self.main_layout.addWidget(self.input_label)
-        self.input_text = qtw.QTextEdit()
+        self.input_text: qtw.QTextEdit = qtw.QTextEdit()
         self.main_layout.addWidget(self.input_text)
 
-        self.formatted_input_label = qtw.QLabel("formatted input")
+        self.formatted_input_label: qtw.QLabel = qtw.QLabel("formatted input")
         self.main_layout.addWidget(self.formatted_input_label)
-        self.formatted_input_text = qtw.QTextEdit()
+        self.formatted_input_text: qtw.QTextEdit = qtw.QTextEdit()
         self.formatted_input_text.setReadOnly(True)
         self.main_layout.addWidget(self.formatted_input_text)
 
-        v_box_layout = qtw.QHBoxLayout()
+        v_box_layout: qtw.QHBoxLayout = qtw.QHBoxLayout()
 
-        output_label = qtw.QLabel("output")
+        output_label: qtw.QLabel = qtw.QLabel("output")
         self.main_layout.addWidget(output_label)
-        self.output_text = qtw.QTextEdit()
+        self.output_text: qtw.QTextEdit = qtw.QTextEdit()
         self.output_text.setReadOnly(True)
         v_box_layout.addWidget(self.output_text)
         v_box_layout.addWidget(self.key_matrix_table)
         self.main_layout.addLayout(v_box_layout)
 
-        matrix = create_key_matrix(self.key_text.text(), config["alphabet"])
+        matrix: list[list[str]] = create_key_matrix(self.key_text.text(), config["alphabet"])
         
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                item = qtw.QTableWidgetItem(matrix[i][j])
+                item: qtw.QTableWidgetItem = qtw.QTableWidgetItem(matrix[i][j])
                 item.setTextAlignment(qtc.Qt.AlignmentFlag.AlignCenter)
                 self.key_matrix_table.setItem(i, j, item)
 
-        button_layout = qtw.QHBoxLayout()
+        button_layout: qtw.QHBoxLayout = qtw.QHBoxLayout()
 
-        encrypt_button = qtw.QPushButton("encrypt")
+        encrypt_button: qtw.QPushButton = qtw.QPushButton("encrypt")
         encrypt_button.clicked.connect(self.encrypt)
         button_layout.addWidget(encrypt_button)
 
-        decrypt_button = qtw.QPushButton("decrypt")
+        decrypt_button: qtw.QPushButton = qtw.QPushButton("decrypt")
         decrypt_button.clicked.connect(self.decrypt)
         button_layout.addWidget(decrypt_button)
 
         self.main_layout.addLayout(button_layout)
 
-    def encrypt(self):
+    def encrypt(self) -> None:
         self.update_key_matrix()
-        encrypted_text = encrypt(
+        encrypted_text: str = encrypt(
             self.key_text.text(), 
             config["alphabet"], 
             self.input_text.toPlainText()
         )
         self.output_text.setText(encrypted_text.upper())
 
-        formatted_input = format_text(self.input_text.toPlainText()).upper()
-        formatted_input = ''.join([
-            c if i == 0 or i % 2 != 0
-            else f' {c}'
-            for i, c in enumerate(formatted_input)
-        ])
+        formatted_input: str = format_text(self.input_text.toPlainText()).upper()
+        formatted_input = tf.groups_of(formatted_input, 2)
         self.formatted_input_text.setText(formatted_input)
 
-    def decrypt(self):
+    def decrypt(self) -> None:
         self.update_key_matrix()
-        decrypted_text = decrypt(
+        decrypted_text: str = decrypt(
             self.key_text.text(), 
             config["alphabet"],
             self.input_text.toPlainText()
         )
         self.output_text.setText(decrypted_text.upper())
 
-        formatted_input = self.input_text.toPlainText().upper()
-        formatted_input = ''.join([
-            c if i == 0 or i % 2 != 0
-            else f' {c}'
-            for i, c in enumerate(formatted_input)
-        ])
+        formatted_input: str = self.input_text.toPlainText().upper()
+        formatted_input = tf.groups_of(formatted_input, 2)
         self.formatted_input_text.setText(formatted_input)
 
-    def update_key_matrix(self):
-        matrix = create_key_matrix(self.key_text.text(), config["alphabet"])
+    def update_key_matrix(self) -> None:
+        matrix: list[list[str]] = create_key_matrix(self.key_text.text(), config["alphabet"])
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 item = qtw.QTableWidgetItem(matrix[i][j])
@@ -228,7 +191,7 @@ class App(qtw.QMainWindow):
                 self.key_matrix_table.setItem(i, j, item)
 
 
-    def clear_layout(self, layout: qtw.QLayout | None): 
+    def clear_layout(self, layout: qtw.QLayout | None) -> None: 
         if layout is None: return
         while layout.count():
             item = layout.takeAt(0)
